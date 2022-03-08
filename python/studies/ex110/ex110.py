9ec89eedf749fede8096695de8c27bb118b1e1dc
Willian Ayres <70988977+willianayres@users.noreply.github.com>
2022-03-08 18:19:11 -0300

    Delete python/studies directory

--- python/studies/ex110/ex110.py
+++
@@ -1,9 +0,0 @@
-"""
-110) Adicione ao módulo moeda.py criado nos exercícios anteriores, uma função chamada resumo(),
-que mostre na tela algumas informações geradas pelas funções que ja temos no módulo criado até
-aqui.
-"""
-import moeda
-
-p = float(input('Digite o preço: R$'))
-moeda.resumo(p, 80, 35)
--- python/studies/ex111/ex111.py
+++
@@ -1,9 +0,0 @@
-"""
-111) Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
-Transfira todas as funções utilizadas nos exercícios 107, 108, e 109 para o primeiro pacote e
-mantenha tudo funcionando.
-"""
-from utilidadescev import moeda
-
-p = float(input('Digite o preço: R$'))
-moeda.resumo(p, 80, 35)
