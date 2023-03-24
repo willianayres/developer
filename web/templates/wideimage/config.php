ab13ff6696824ce3f45d65a48e99f26086a730da
Willian Ayres <70988977+willianayres@users.noreply.github.com>
2023-03-24 15:04:34 -0300

    Add files via upload

---
+++ web/templates/wideimage/config.php
@@ -0,0 +1,23 @@
+<?php
+
+	// Star a new session to login.
+	session_start();
+
+	// Set the timezone.
+	date_default_timezone_set('America/Sao_Paulo');
+
+	// Autoload all the classes.
+	$autoload = function($class){	
+		include('classes/'.$class.'.php');
+	};
+	spl_autoload_register($autoload);
+
+	// Conection to the Database.
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASSWORD','');
+	define('DATABASE','test');
+
+	// Root Paths.
+	define('INCLUDE_PATH','http://localhost/Detector_OS/');
+?>
