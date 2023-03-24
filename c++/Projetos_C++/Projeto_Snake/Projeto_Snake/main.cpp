f5ecba712be3ae9c044d5e92ef15bbfd2c517f45
Willian Ayres <70988977+willianayres@users.noreply.github.com>
2023-03-24 16:42:45 -0300

    Delete c++ directory

--- c++/Projetos_C++/Projeto_Snake/Projeto_Snake/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "game.h" //Biblioteca com as funções do jogo//
-
-int main()        //Função main//
-{
-    system("title Snake");
-    menu();       //Função que inicializa o menu//
-    return 0;     //Retorna da main//
-}
--- c++/Projetos_C++/Projeto_Snake/Projeto_Snake/position.h
+++
@@ -1,9 +0,0 @@
-#ifndef POSITION_H_INCLUDED
-#define POSITION_H_INCLUDED
-
-#include <Windows.h>
-
-//Move o cursor no console para posição específica.//
-void goto_XY(short,short);
-
-#endif // POSITION_H_INCLUDED
